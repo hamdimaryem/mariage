6ad9561273e6b865f3c13645d662c511f8e6e27e
hamdimaryem <elhamdimariem6@gmail.com>
2024-03-23 16:35:59 +0100

    site de mariage modifié

---
+++ src/app/fleuriste/fleuriste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fleuriste',
+  standalone: true,
+  imports: [],
+  templateUrl: './fleuriste.component.html',
+  styleUrl: './fleuriste.component.css'
+})
+export class FleuristeComponent {
+
+}
