63d5171f8ac869b51f934e9806f3385806266f75
hamdimaryem <elhamdimariem6@gmail.com>
2024-03-24 01:48:23 +0100

    ajout interface fleuriste fait version 1

--- src/app/fleuriste/fleuriste.component.ts
+++ src/app/fleuriste/fleuriste.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
 
+interface Fleuriste {
+  nom: string;
+  lieu: string;
+  avis: number;
+  prixMoyen: number;
+  email: string;
+  numeroTelephone: string;
+  services: string[];
+  image: File;
+}
+
 @Component({
   selector: 'app-fleuriste',
-  standalone: true,
-  imports: [],
   templateUrl: './fleuriste.component.html',
-  styleUrl: './fleuriste.component.css'
+  styleUrls: ['./fleuriste.component.css']
 })
 export class FleuristeComponent {
 
-}
+  nom: string;
+  lieu: string;
+  avis: number;
+  prixMoyen: number;
+  email: string;
+  numeroTelephone: string;
+  services: string[];
+  image: File;
+  fleuristeAjoute: boolean = false; // Initialisation de la variable fleuristeAjoute à false
+  fleuristes: Fleuriste[] = []; // Tableau pour stocker tous les fleuristes
+
+  constructor() { }
+
+  ajouterPrestataire() {
+    // Vérifier si tous les champs sont remplis
+    if (!this.nom || !this.lieu || !this.avis || !this.prixMoyen || !this.email || !this.numeroTelephone || !this.services || !this.image) {
+      alert("Veuillez remplir tous les champs.");
+      return;
+    }
+
+    // Vérifier si l'avis est dans la plage valide (0 à 5)
+    if (this.avis < 0 || this.avis > 5) {
+      alert("L'avis doit être compris entre 0 et 5.");
+      return;
+    }
+
+    // Créer un nouvel objet Fleuriste avec les données saisies
+    const nouveauFleuriste: Fleuriste = {
+      nom: this.nom,
+      lieu: this.lieu,
+      avis: this.avis,
+      prixMoyen: this.prixMoyen,
+      email: this.email,
+      numeroTelephone: this.numeroTelephone,
+      services: this.services,
+      image: this.image
+    };
+
+    // Ajouter le nouveau fleuriste au tableau des fleuristes
+    this.fleuristes.push(nouveauFleuriste);
+
+    // Mettre à jour la variable fleuristeAjoute pour afficher le message de succès
+    this.fleuristeAjoute = true;
+
+   
